4fdd6b27be61de32581c3fa1ed53d97a241271be
Peter Vorman <pvorman@opengov.com>
2025-03-24 16:21:28 +0200

    markdown-it

--- ckanext/markdown_view/assets/md_view.js
+++ ckanext/markdown_view/assets/md_view.js
@@ -2,11 +2,14 @@ function render_md(url, target_element) {
   fetch(url)
     .then(response => response.text())
     .then(text => {
-            converter = new showdown.Converter({
-                'tables': true,
-                'smoothLivePreview': true
+            converter = new markdownit({
+                html: false,   // Allow HTML tags in Markdown
+                linkify: true, // Auto-detect links
+                typographer: true // Convert quotes to typographic symbols
             });
-            html= converter.makeHtml(text);
+            converter.use(window.markdownitFootnote);
+            converter.use(window.markdownItAttrs);
+            html= converter.render(text);
             target_element.innerHTML = html;
   })
   .catch(console.error);
--- ckanext/markdown_view/tests/test_view.py
+++ ckanext/markdown_view/tests/test_view.py
@@ -11,18 +11,6 @@ def _add_responses_solr_passthru():
     responses.add_passthru(tk.config.get('solr_url'))
 
 
-def pytest_generate_tests(metafunc):
-    # called once per each test function
-    if metafunc.cls is not TestMDView:
-        return
-    funcarglist = metafunc.cls.params[metafunc.function.__name__]
-
-    argnames = sorted(funcarglist[0])
-    metafunc.parametrize(
-        argnames, [[funcargs[name] for name in argnames] for funcargs in funcarglist]
-    )
-
-
 @pytest.mark.usefixtures('clean_db', 'clean_index', 'with_plugins', 'with_request_context')
 @pytest.mark.ckan_config('ckan.plugins', 'datastore markdown_view')
 def test_view_create():
@@ -49,64 +37,3 @@ def test_view_create():
     )
 
     assert response.get('view_type') == 'Markdown'
-
-
-@pytest.mark.usefixtures('clean_db', 'clean_index', 'with_plugins', 'with_request_context')
-@pytest.mark.ckan_config('ckan.plugins', 'datastore markdown_view')
-class TestMDView:
-    params = {
-        'test_rendering': [
-            dict(file_content='[Test link in new tab](http://www.example.org/){: target=”_blank”}',
-                 html_entries=['<a href="http://www.example.org/" target="”_blank”">Test link in new tab</a>']),
-            dict(file_content='[Test link](http://www.example.org/)',
-                 html_entries=['<a href="http://www.example.org/">Test link</a>']),
-            dict(file_content='| Col1 | Col2 |\n'
-                              '|------|------|\n'
-                              '| val1 | val2 |\n',
-                 html_entries=['<table>',
-                               '<thead>',
-                               '<tr>',
-                               '<th>Col1</th>',
-                               '<th>Col2</th>',
-                               '</tr>',
-                               '</thead>',
-                               '<tbody>',
-                               '<tr>',
-                               '<td>val1</td>',
-                               '<td>val2</td>',
-                               '</tr>',
-                               '</tbody>',
-                               '</table>',
-                               ]
-                 ),
-
-        ]
-    }
-
-    @responses.activate
-    def test_rendering(self, file_content, html_entries, app):
-        _add_responses_solr_passthru()
-        file = file_content
-        responses.get('http://link.to.some.data/', body=file)
-
-        user = factories.Sysadmin()
-        org = factories.Organization()
-        env = {"REMOTE_USER": user['name'].encode('ascii')}
-        dataset = factories.Dataset(owner_org=org['id'])
-        res = factories.Resource(user=user, format='abc', package_id=dataset['id'])
-
-        resource_view = factories.ResourceView(
-            resource_id=res['id'],
-            view_type='Markdown'
-        )
-
-        url = tk.url_for('resource.view',
-                         id=dataset["id"],
-                         resource_id=res.get("id"),
-                         view_id=resource_view.get('id'),
-                         qualified=False
-                         )
-
-        get_response = app.get(url, extra_environ=env)
-        for entry in html_entries:
-            assert entry in get_response
